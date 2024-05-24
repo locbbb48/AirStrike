85b298e94f7282542888b1519d159216c2fa22e6
locbbb48 <139316884+locbbb48@users.noreply.github.com>
2024-05-24 22:48:49 +0700

    Assets
    Initial

---
+++ Assets/MainGame/Script/PlaneAbstract.cs
@@ -0,0 +1,16 @@
+/*	  - Codeby Bui Thanh Loc -
+    contact : builoc08042004@gmail.com
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlaneAbstract : MonoBehaviour
+{
+    public float MainSpeed;
+    public float HPmax;
+    public float HPrealtime;
+
+    public float FireSpeed;
+}
---
+++ Assets/MainGame/Script/PlanePlayer.cs
@@ -0,0 +1,31 @@
+/*	  - Codeby Bui Thanh Loc -
+	contact : builoc08042004@gmail.com
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePlayer : PlaneAbstract
+{
+    private void Start()
+    {
+              
+    }
+
+    private void Update()
+    {
+        if (this.gameObject != null)
+        {
+
+        }
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) 
+        {
+            transform.position.
+        }
+    }
+}
