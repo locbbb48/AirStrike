073c4b9dc4f65a480a3feb6d8d092e161f33f4a4
locbbb48 <builoc08042004@gmail.com>
2024-05-26 23:22:22 +0700

    Create PlaneAbstract.cs

---
+++ Assets/MainGame/Script/Plane/PlaneAbstract.cs
@@ -0,0 +1,15 @@
+/*	  - Codeby Bui Thanh Loc -
+    contact : builoc08042004@gmail.com
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlaneAbstract : MonoBehaviour
+{
+    public float MainSpeed;
+    public float maxHP;
+    public float currentHP;
+    public float FireSpeed = 10f;
+}
