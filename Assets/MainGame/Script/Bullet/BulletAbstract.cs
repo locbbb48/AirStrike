11deb4d60c0177ce030c2ea22d2aead0fa82b7f9
locbbb48 <builoc08042004@gmail.com>
2024-05-26 23:20:43 +0700

    Create BulletAbstract.cs
    init

---
+++ Assets/MainGame/Script/Bullet/BulletAbstract.cs
@@ -0,0 +1,38 @@
+﻿/*	  - Codeby Bui Thanh Loc -
+	contact : builoc08042004@gmail.com
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAbstract : MonoBehaviour
+{
+    public float Speed;
+    public bool isColl = false;
+    public bool isSetActive = false;
+
+    public float maxDistance = 10f;
+
+    public Vector3 startPosition;
+
+    public void OnEnable()
+    {
+        isSetActive = true;
+    }
+
+    public virtual void Update()
+    {
+        DeActivebyDistance();
+    }
+
+    public void DeActivebyDistance()
+    {
+        if (this.transform.position.y >= maxDistance)
+        {
+            isSetActive = false;
+            BulletPlayer bullet = this.gameObject.GetComponent<BulletPlayer>();
+            GameManager.Instance.bulletPlayerPool.ReturnObject(bullet) ;
+        }
+    }
+}
